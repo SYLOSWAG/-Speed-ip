d89ad70e05d356ae865a62b36f176e9f63a6c6fa
SYLOSWAG <158841460+SYLOSWAG@users.noreply.github.com>
2024-03-09 11:30:26 +0530

    Update install.py

--- install.py
+++ install.py
@@ -11,7 +11,7 @@ if str(choice) =='Y' or str(choice)=='y':
     with open('/usr/bin/aut','w')as file:
         file.write(cmnd)
     run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/Speed.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    print('''\n\ncongratulation Speed ip is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
     run('rm -r /usr/share/aut ')
     run('rm /usr/bin/aut ')
